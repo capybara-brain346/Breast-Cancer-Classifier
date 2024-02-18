95d10bd0c6c93e0f992b38dad0758cd1f9f3441d
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-02-18 22:17:10 +0530

    fixed pickle file path

--- flask-api/app.py
+++ flask-api/app.py
@@ -5,27 +5,30 @@ import pickle
 
 app = Flask(__name__)
 
+
 def load_model():
-    model = pickle.load(open(r'model\random_forest_model.pkl', 'rb'))
+    model = pickle.load(open(r'..\model\model\random_forest_model.pkl', 'rb'))
     return model
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     int_features = [int(float(x)) for x in request.form.values()]
     final_features = [np.asarray(int_features)]
     predicted_class = load_model().predict(final_features)
     prediction = ""
-    if predicted_class==0:
-        prediction = prediction+"Benign Cancer"
-    else: 
-        prediction = prediction+"Malignant Cancer"
-        
+    if predicted_class == 0:
+        prediction = prediction + "Benign Cancer"
+    else:
+        prediction = prediction + "Malignant Cancer"
+
     return render_template('index.html', prediction_text=f"You're breast cancer is a {prediction}")
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
--- model/model.py
+++ model/model.py
@@ -1,35 +1,42 @@
 import os
 import pandas as pd
-import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-class model:
+
+class Model:
     def __init__(self, data) -> None:
         self.data = data
-        
-    def data_spliting(self):
-        x = self.data.drop(['target'],axis=1)
+        self.random_forest_model = None
+        self.y_test = None
+        self.y_train = None
+        self.x_test = None
+        self.x_train = None
+
+    def data_splitting(self):
+        x = self.data.drop(['target'], axis=1)
         y = self.data['target']
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.4, random_state=60)
 
     def train(self):
-        self.data_spliting()
+        self.data_splitting()
         self.random_forest_model = RandomForestClassifier(max_depth=3).fit(self.x_train, self.y_train)
         return self.random_forest_model
-    
+
     def final_build(self):
         if not os.path.exists("model"):
             os.makedirs("model")
-        with open("model/random_forest_model.pkl", "wb") as file:
-            pickle.dump(self.random_forest_model, file)    
-        
-def main():    
-    model_data = pd.read_csv(r"data\model_training_data.csv")
-    Breast_Cancer_Classifier = model(data=model_data)
+        with open(r"model\random_forest_model.pkl", "wb") as file:
+            pickle.dump(self.random_forest_model, file)
+
+
+def main():
+    model_data = pd.read_csv(r"..\data\model_training_data.csv")
+    Breast_Cancer_Classifier = Model(data=model_data)
     trained_model = Breast_Cancer_Classifier.train()
     Breast_Cancer_Classifier.final_build()
-       
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
