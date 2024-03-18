8519a6833294155d041feb0419596d876336d2b3
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-03-18 10:59:21 +0530

    created angular component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './routes'; // Import the routes array
+// Import components if needed
 
-export const routes: Routes = [];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
